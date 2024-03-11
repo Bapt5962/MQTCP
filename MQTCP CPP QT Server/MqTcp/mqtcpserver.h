fc99f72a4cf624530f09a9dfa7df634f6ce8719f
Bapt59 <bapt5962@gmail.com>
2024-03-11 09:42:05 +0100

    Added licences comments with version numbers

--- MQTCP CPP QT Server/MqTcp/mqtcpserver.h
+++ MQTCP CPP QT Server/MqTcp/mqtcpserver.h
@@ -1,3 +1,27 @@
+/*
+mqtcpserver.h
+
+Version: 1.0.0
+Protocol Version: 1
+
+This file is part of MQTCP CPP Qt Server.
+
+MQTCP CPP Qt Server is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MQTCP CPP Qt Server is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MQTCP CPP Qt Server. If not, see <http://www.gnu.org/licenses/>.
+
+Author: Bapt59
+*/
+
 #ifndef MQTCPSERVER_H
 #define MQTCPSERVER_H
 
